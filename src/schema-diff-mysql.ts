71101aba475af8e2fc7aa52a3eb96cbfc146ce97
Baptiste Pillot <baptiste@pillot.fr>
2025-10-09 17:55:58 +0200

    Sort columns by name, Prefer KEY keyword

--- src/schema-diff-mysql.ts
+++ src/schema-diff-mysql.ts
@@ -9,26 +9,70 @@ export class SchemaDiffMysql extends SchemaToMysql
 
 	alterColumnsSql(tableDiff: TableDiff, deletion = false)
 	{
+		let   hasId = false
+		const setColumns: string[] = []
+		for (const column of tableDiff.source.columns) {
+			if (column.name === 'id') {
+				hasId = true
+				continue
+			}
+			setColumns.push(column.name)
+		}
+		setColumns.sort()
+		tableDiff.additions.sort((column1, column2) => {
+			if (column1.name === 'id') return -1
+			if (column2.name === 'id') return 1
+			return (column2.name > column1.name) ? 1 : -1
+		})
+
 		const sql = new Array<string>()
+
 		if (deletion) {
 			for (const column of tableDiff.deletions) {
 				if (!(column instanceof Column)) continue
 				sql.push('DROP COLUMN `' + column.name + '`')
 			}
 		}
+
 		for (const columnDiff of tableDiff.changes) {
 			if (!(columnDiff.source instanceof Column) || !(columnDiff.target instanceof Column)) continue
 			if (columnDiff.source.name === columnDiff.target.name) {
 				sql.push('MODIFY COLUMN ' + this.columnSql(columnDiff.target))
 			}
 			else {
-				sql.push('CHANGE COLUMN `' + columnDiff.source.name + '` ' + this.columnSql(columnDiff.target))
+				let afterColumn = ''
+				let sourceName  = columnDiff.source.name
+				let targetName  = columnDiff.target.name
+				if (targetName !== 'id') {
+					for (const otherColumn of setColumns) {
+						if (otherColumn === sourceName) continue
+						if (otherColumn > targetName) break
+						afterColumn = otherColumn
+					}
+				}
+				sql.push(
+					'CHANGE COLUMN `' + sourceName + '` ' + this.columnSql(columnDiff.target)
+					+ ((afterColumn === '') ? (hasId ? ' AFTER `id`' : ' FIRST') : (' AFTER `' + afterColumn + '`'))
+				)
 			}
 		}
+
 		for (const column of tableDiff.additions) {
 			if (!(column instanceof Column)) continue
-			sql.push('ADD COLUMN ' + this.columnSql(column))
+			let afterColumn = ''
+			let columnName  = column.name
+			if (columnName !== 'id') {
+				for (const otherColumn of setColumns) {
+					if (otherColumn > columnName) break
+					afterColumn = otherColumn
+				}
+			}
+			sql.push(
+				'ADD COLUMN ' + this.columnSql(column)
+				+ ((afterColumn === '') ? (hasId ? ' AFTER `id`' : ' FIRST') : (' AFTER `' + afterColumn + '`'))
+			)
 		}
+
 		return sql.join(',\n')
 	}
 
@@ -39,20 +83,20 @@ export class SchemaDiffMysql extends SchemaToMysql
 			for (const index of tableDiff.deletions) {
 				if (!(index instanceof Index)) continue
 				if (index.type === 'primary') {
-					sql.push('DROP PRIMARY INDEX')
+					sql.push('DROP PRIMARY KEY')
 				}
 				else {
-					sql.push('DROP INDEX `' + index.name + '`')
+					sql.push('DROP KEY `' + index.name + '`')
 				}
 			}
 		}
 		for (const indexDiff of tableDiff.changes) {
 			if (!(indexDiff.source instanceof Index) || !(indexDiff.target instanceof Index)) continue
 			if (indexDiff.source.type === 'primary') {
-				sql.push('DROP PRIMARY INDEX')
+				sql.push('DROP PRIMARY KEY')
 			}
 			else {
-				sql.push('DROP INDEX `' + indexDiff.source.name + '`')
+				sql.push('DROP KEY `' + indexDiff.source.name + '`')
 			}
 			sql.push('ADD ' + this.indexSql(indexDiff.target))
 		}
